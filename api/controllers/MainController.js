90b2d695d02afec26d1446e694d7e272e9cc4e55
Jon Vaughn <jon@thinkvaughn.com>
2018-02-23 03:20:12 +0530

    revert

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -59,40 +59,7 @@ module.exports = {
     }
 	
 	
-	getData2: function (req, res) {
-  		// set the parameter for the request message
-		const configs = {
-		    method: 'GET',
-		    path: '/v2/exchange-rates?currency=USD',
-		    body: ''
-		};
 	
-
-		const message = timestamp + configs.method + configs.path + configs.body;
-		console.log(message);
-
-		//create a hexedecimal encoded SHA256 signature of the message
-		const signature = crypto.createHmac("sha256", apiSecret).update(message).digest("hex");
-
-		//create the request options object
-		const options = {
-		    baseUrl: 'https://api.coinbase.com/',
-		    url: configs.path,
-		    method: configs.method,
-		    headers: {
-		        'CB-ACCESS-SIGN': signature,
-		        'CB-ACCESS-TIMESTAMP': timestamp,
-		        'CB-ACCESS-KEY': apiKey,
-		        'CB-VERSION': '2015-07-22'
-		    }
-		};
-
-		request(options,function(err, response){
-		    if (err) console.log(err);
-		    console.log(response.body);
-		    return res.send(response.body);
-		});
-    }
 	
 	
 }
