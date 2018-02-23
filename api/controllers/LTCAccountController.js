f2378f632552931e5c7c94f360321c4451c5b252
Jon Vaughn <jon@thinkvaughn.com>
2018-02-23 06:42:53 +0530

    new

--- api/controllers/LTCAccountController.js
+++ api/controllers/LTCAccountController.js
@@ -62,7 +62,7 @@ module.exports = {
 		var body = JSON.parse(response.body);		
 		console.log(body);
 
-		//var cf = '{ "messages": [{"text" : "The current price of 1 '+body.data.base +' is $'+body.data.amount +'"} ]}';
+		var cf = '{ "messages": [{"text" : "You have a total of '+body.balance.amount +' '+body.balance.currency +' that is worth a total of $'+ body.native_balance.amount+ '"} ]}';
 
 	    return res.send(body);
 		});
