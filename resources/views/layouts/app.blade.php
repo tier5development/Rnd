f01a0597a8830cfdaabea0ac17c79db1ce1cedcc
Tier5 LLC <jon@thinkvaughn.com>
2018-07-31 21:27:28 +0530

    Configuration V1.0 stable released to dev

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Article') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
  */
 
-/*Route::get('/', function () {
-return view('welcome');
+Route::get('/', function () {
+    return view('welcome');
 });
- */
+ 
 Route::get('registration', 'UserController@create');
 Route::post('/registration', 'UserController@store')->name('register');
 
