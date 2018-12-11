ef4909a7bcc4848092884dccdacac427d04d6c13
gautam <gautam.kumar@tier5.in>
2018-12-11 15:19:32 +0530

    updated the code

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 // This Code is Modified under <feature_branch_by_gautam>
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'feature_branch_by_gautam' });
+  res.render('index', { title: 'Code' });
 });
 
 
