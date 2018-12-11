90469925a57640711616e7efffc078141017f7ee
gautam <gautam.kumar@tier5.in>
2018-12-11 12:29:13 +0530

    This code is modified under feature_branch_by_gautam

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// This Code is Modified under <feature_branch_by_gautam>
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'feature_branch_by_gautam' });
 });
 
 module.exports = router;
