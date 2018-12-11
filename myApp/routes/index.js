1b4871db9a69041535eaf91d36bfa0c4f48b637a
gautam <gautam.kumar@tier5.in>
2018-12-11 12:59:21 +0530

    edited file

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -6,8 +6,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'feature_branch_by_gautam' });
 });
-router.get('/new', function(req, res, next) {
-  res.render('index', { title: 'New Route from rnd_by_gautam branch' });
-});
+
 
 module.exports = router;
