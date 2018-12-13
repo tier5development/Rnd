ab3a27cccea9d40c3e5158deb2c643e896357068
Tier5 <work@tier5.us>
2018-12-13 14:18:59 +0530

    csv download

--- content.js
+++ content.js
@@ -1,26 +1,32 @@
 chrome.runtime.onMessage.addListener(
 
-    function(request, sender, sendResponse) {
+	function (request, sender, sendResponse) {
+		if (request.greeting == "hello") {
+			var StrObj = document.documentElement.innerText;
+			var emailsArray = StrObj.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+			if (emailsArray != null && emailsArray.length) {
 
-        var StrObj = document.documentElement.innerText;
-        var emailsArray = StrObj.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-        if (emailsArray != null && emailsArray.length) {
-            console.log(emailsArray);
-        } else {
-            console.log("no email found");
+				var csvHeader = ["email", "phno"];
+				csvHeader.join(',');
+				var csvData = [];
+				csvData.push(csvHeader);
+				csvData.push(emailsArray.join("\n"));
+				var csvRow = csvData.join("\n");
+				var hiddenElement = document.createElement('a');
+				hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvRow);
+				hiddenElement.target = '_blank';
+				hiddenElement.download = 'data.csv';
+				hiddenElement.click();
 
-        }
+			} else {
+				alert("no email found");
+			}
+			var phoneArray = StrObj.match(/\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/);
+			if (phoneArray != null && phoneArray.length) {
+				console.log(phoneArray);
+			} else {
+				console.log("no ph no found");
+			}
+		}
 
-        var phoneArray = StrObj.match(/\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/);
-        if (phoneArray != null && phoneArray.length) {
-            console.log(phoneArray);
-        } else {
-            console.log("no ph no found");
-        }
-
-
-        if (request.greeting == "hello")
-            sendResponse({
-                farewell: "goodbye"
-            })
-    });
+	});
