2138097b71bf22c5fd865239e4e7f4f46f27b12f
jon vaughn <jon@thinkvaughn.com>
2019-07-09 16:36:23 +0530

    V1.0

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+// Use body-parser to retrieve the raw body as a buffer
+const bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Match the raw body to content type application/json
+router.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+  let event;
+
+  try {
+    event = JSON.parse(request.body);
+  }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  console.log("event :: ", JSON.stringify(event.type))
+
+  // // Handle the event
+  // switch (event.type) {
+  //   case 'payment_intent.succeeded':
+  //     const paymentIntent = event.data.object;
+  //     handlePaymentIntentSucceeded(paymentIntent);
+  //     break;
+  //   case 'payment_method.attached':
+  //     const paymentMethod = event.data.object;
+  //     handlePaymentMethodAttached(paymentMethod);
+  //     break;
+  //   // ... handle other event types
+  //   default:
+  //     // Unexpected event type
+  //     return response.status(400).end();
+  // }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({received: true});
 });
 
 module.exports = router;
