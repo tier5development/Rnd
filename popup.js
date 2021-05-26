afc1b097cee466f731bd0eec443b06c6e76bc456
Manisha <manishadey2020@gmail.com>
2021-05-26 15:48:20 +0530

    This is my fourth commit

--- popup.js
+++ popup.js
@@ -1,93 +1,97 @@
-var listArray=[];
-function init(){
-   document.getElementById("tablerows").innerHTML="";
-   if (localStorage.allTasks){
-       listArray=JSON.parse(localStorage.allTasks);
-       for(var i=0; i<listArray.length;i++ ){
-           prepareTableCell(i,listArray[i].name,listArray[i].category)
-       }  
-   }
+var listArray = [];
+function init() {
+    document.getElementById("tablerows").innerHTML = "";
+    if (localStorage.allTasks) {
+        listArray = JSON.parse(localStorage.allTasks);
+        for (var i = 0; i < listArray.length; i++) {
+            prepareTableCell(i, listArray[i].name, listArray[i].category);
+        }
+    }
 }
 //When add product button clicked
-document.querySelector(".button").addEventListener("click" , function() {
-    document.querySelector(".container").style.display='block';
+document.querySelector(".button").addEventListener("click", function () {
+    document.querySelector(".container").style.display = "block";
     var div = document.getElementById("main");
     div.style.display = "block";
-})
+});
 //when submit button clicked
-document.querySelector(".submit").addEventListener("click" , function() {
-    var pName= document.getElementById("name").value;
-    var pCategory= document.getElementById("category").value;
-    var listObj={name:pName,category:pCategory}
-       if(selectedIndex===-1){
-           listArray.push(listObj);
-       }else{
-           listArray.splice(selectedIndex,1,listObj);
-       }
-       localStorage.allTasks=JSON.stringify(listArray);
-       init();
-       document.getElementById("name").value="";
-       document.getElementById("category").value="";
+document.querySelector(".submit").addEventListener("click", function () {
+    var pName = document.getElementById("name").value;
+    var pCategory = document.getElementById("category").value;
+    var listObj = { name: pName, category: pCategory };
+    if (selectedIndex === -1) {
+        listArray.push(listObj);
+    } else {
+        listArray.splice(selectedIndex, 1, listObj);
+    }
+    localStorage.allTasks = JSON.stringify(listArray);
+    init();
+    document.getElementById("name").value = "";
+    document.getElementById("category").value = "";
     var div = document.getElementById("main");
     div.style.display = "none";
-})
-function prepareTableCell(index,pName,pCategory){
-   var table=document.getElementById("tablerows");
-   var row=table.insertRow();
-   var pNameCell=row.insertCell(0);
-   var pCategoryCell=row.insertCell(1);
-   var actionCell=row.insertCell(2);
+});
+function prepareTableCell(index, pName, pCategory) {
+    var table = document.getElementById("tablerows");
+    var row = table.insertRow();
+    var pNameCell = row.insertCell(0);
+    var pCategoryCell = row.insertCell(1);
+    var actionCell = row.insertCell(2);
 
-   pNameCell.innerHTML=pName;
-   pCategoryCell.innerHTML=pCategory;
-   actionCell.innerHTML='<button class="onEdit"('+index+')>Edit</button><button class="deleteTableRow"('+index+')>Delete</button>';
+    pNameCell.innerHTML = pName;
+    pCategoryCell.innerHTML = pCategory;
+    actionCell.innerHTML = '<button id="sam" class="onEdit(' + index + ')">Edit</button><button id="magic" class="deleteTableRow(' + index + ')" >Delete</button>';
+}
 
+var tar = document.getElementById("magic");
+if (tar) {
+    tar.addEventListener(
+        "click",
+        function (index) {
+            listArray.splice(index, 1);
+            localStorage.allTasks = JSON.stringify(listArray);
+            init();
+            console.log("hii");
+        },
+        false
+    );
 }
-function deleteTableRow(index){
-   listArray.splice(index,1);
-   localStorage.allTasks=JSON.stringify(listArray);
-   init(); 
-}
-var selectedIndex=-1;
-document.querySelector(".onEdit").addEventListener("click" , function(index) {
+//  function deleteTableRow(index){
+//    listArray.splice(index,1);
+//    localStorage.allTasks=JSON.stringify(listArray);
+//    init();
+
+var selectedIndex = -1;
+// document.querySelector(".onEdit('+index+')").addEventListener("click" , function(index) {
+//     index.preventDefault()
 // function onEdit(index){
-   selectedIndex=index;
-   var listObj=listArray[index];
-   document.getElementById("name").value=listObj.name;
-   document.getElementById("category").value=listObj.category;
-    var div = document.getElementById("main");
-    div.style.display = "block";
-    document.getElementById("submit").innerHTML="Update";
-    init();
-})
+var rar = document.getElementById("sam");
+if (rar) {
+    tar.addEventListener(
+        "click",
+        function (index) {
+            console.log("hii");
+            selectedIndex = index;
+            var listObj = listArray[index];
+            document.getElementById("name").value = listObj.name;
+            document.getElementById("category").value = listObj.category;
+            var div = document.getElementById("main");
+            div.style.display = "block";
+            document.getElementById("submit").innerHTML = "Update";
+            init();
+        },
+        false
+    );
+}
 
-
-
-// var form = document.querySelector("#form");
-// form.addEventListener("submit" , function(event) {
-//     event.preventDefault()
-//     var username=parseInt(document.querySelector("#username").value);
-//     console.log(username)
-
-//     // send message from popup to content
-//     chrome.tabs.query( { active : true, currentWindow : true }, function (tabs) {
-//         chrome.tabs.executeScript(tabs[0].id, { file : "content.js"},
-//         function(){
-//             chrome.tabs.sendMessage(tabs[0].id, {
-//                 number : username
-//             })
-//         })
-//     })
-
-// });
-
-
-
-// const sendRequest = (data) => {
-//     try {
-        
-//     }
-//     catch(ext) {
-//         alert("please reload ");
-//     }
-// }
+//  window.onclick=function(event){
+//      var target= event.target;
+//      if(target.matches('.deleteTableRow')){
+//         //  var deleteEle=document.activeElement.id;
+//         //  var ele=document.getElementById(deleteEle);
+//         // alert(ele.txt);
+//         listArray.splice(index,1);
+//            localStorage.allTasks=JSON.stringify(listArray);
+//            init();
+//      }
+//  }
