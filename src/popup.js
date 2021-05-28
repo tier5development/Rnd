4b7c55aff271f557f79b2a801cf36e29a8afa6c1
Manisha <manishadey2020@gmail.com>
2021-05-28 21:12:26 +0530

    This is my final commit

--- src/popup.js
+++ src/popup.js
@@ -1,34 +1,39 @@
 // create task and display
 
-function createTask(value) {
+    function createTask(value) {
     let newtask = { task: value, id: Date.now()};
     let newTaskHtml = `<li id=${newtask.id}>${newtask.task}<i id=${newtask.id}></i></li>`;
     $('#list').append(newTaskHtml);
+    $("#name").val("");
     let tasks = getTasksFromLocalStorage();
-    let allTasks = [...tasks, newtask];
+    let allTasks = [...tasks,newtask];
     saveTaskTolocalStorage(allTasks);
+   
+   
   }
-  
   
   //  get data from localStorage
   
-  function getTasksFromLocalStorage() {
+    function getTasksFromLocalStorage() {
     let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
     return tasks;
+    
   }
   
   
   //  store data to localStorage
   
-  function saveTaskTolocalStorage(tasks) {
+    function saveTaskTolocalStorage(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
+   
   }
   
   
-  // add event when called
-  
-  $('#button').click(function () {
-    createTask($('#name').val());
+// add event when called
+
+  $(document).ready(function(){
+    $('#button').click(function () {
+    createTask($("#name").val()); 
+   });
   });
-  
   
