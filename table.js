c65d5e18b5c526abb30f5dc8282fb1d9e0a8e6d7
Prashant <prasantakumar@tier5.in>
2022-07-01 22:38:10 +0530

    wip delete

--- table.js
+++ table.js
@@ -1,13 +1,24 @@
 const tblBody = document.getElementById("tblBody");
+const fid = document.getElementById("id");
+const fname = document.getElementById("name");
+const furl = document.getElementById("url")
 
 chrome.storage.local.get(["payload"], function ({payload}) {
     payload.forEach(({id, name, url}) => {
-      const htmlElem = `<tr>
-                        <td><div>${id} </div> <button id="${id}" onClick="removeId()">Delete</button></td>
-                        <td><div>${name} </div> <button id="${name}" onClick="removeId()">Delete</button></td>
-                        <td><div>${url} </div> <button id="${url}" onClick="removeId()">Delete</button></td>
-                        </tr>`;
-      tblBody.innerHTML = htmlElem;
+      // const htmlElem = `<tr>
+      //                   <td><div>${id} </div> <button id="userId" onClick="removeId(event)">Delete</button></td>
+      //                   <td><div>${name} </div> <button id="userName" onClick="removeName()">Delete</button></td>
+      //                   <td><div>${url} </div> <button id="userUrl" onClick="removeUrl()">Delete</button></td>
+      //                   </tr>`;
+      // const row = tblBody.insertRow();
+      // row.insertCell(0).innerHTML = id + `<button id="userId" onClick=removeId(event)>DeleteId</button>`;
+      // row.insertCell(1).innerHTML =name + `<button id="userName" onClick=removeName()>Delete</button>`; 
+      // row.insertCell(2).innerHTML = url + `<button id="userUrl" onClick=removeUrl()>Delete</button>`; 
+      // tblBody.innerHTML = row;
+      fid.innerHTML = id;
+      fname.innerHTML = name;
+      furl.innerHTML = url
+
     });
     // payload.forEach(({id, name, url}) => {
     //   const idOfElement = document.getElementById(`"${id}"`);
@@ -16,6 +27,7 @@ chrome.storage.local.get(["payload"], function ({payload}) {
 });
 
 
-document.getElementById(`"${payload.id}"`).addEventListener("click", function(){
-  console.log("bndjiashjidchji");
-})
+const removeId = (e) => {
+  e.preventDefault();
+  console.log("working");
+}
