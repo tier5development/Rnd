cf663c7ddbe41607ebd0879335a27185e6513ec0
Prashant <prasantakumar@tier5.in>
2022-07-05 12:55:38 +0530

    delete func implemented

--- table.js
+++ table.js
@@ -3,95 +3,91 @@ const tblHead = document.getElementById("tblHead");
 console.log(tblHead);
 // function renderHtml(){
 chrome.storage.local.get(["payload"], function ({ payload }) {
-  console.log("payload data top:::",payload);
+  console.log("payload data top:::", payload);
   console.log("dataaaaaaaa:::", payload.id, payload.name, payload.url);
-  appendTableHead();
-  appendTableBody(payload)
-  const htmlElem = `<tr>
-                        <td id="idData">${payload.id}<button id="userId">Delete</button></td>
-                        <td id="nameData">${payload.name}<button id="userName">Delete</button></td>
-                        <td id="urlData">${payload.url}<button id="userUrl">Delete</button></td>
-                        </tr>`;
-  tblBody.innerHTML = htmlElem;
+  // appendTableHead();
+  appendTableBody(payload);
 
   //Removing the user id
-  document.getElementById("userId").addEventListener("click", function () {
-    console.log("working or not");
-    let items = chrome.storage.local.get(["payload"]);
-    console.log("data contains in items", items);
-    console.log("value of payload", payload);
-    // console.log(keyArr);
-    keyIdRemove("id", payload);
-    if (payload.id == undefined)
-      document.getElementById("idData").style.display = "none";
-  });
+  // document.querySelector("button")
+  // .addEventListener("click", function (e) {
+  //   e.preventDefault();
+  //   console.log("value of event", e);
+  //   console.log("working or not");
+  //   let items = chrome.storage.local.get(["payload"]);
+  //   console.log("data contains in items", items);
+  //   console.log("value of payload", payload);
+  //   // console.log(keyArr);
+  //   keyIdRemove("id", payload);
+  //   if (payload.id == undefined)
+  //     document.getElementById("idData").style.display = "none";
+  // });
   //Removing the user name
-  document.getElementById("userName").addEventListener("click", function () {
-    console.log("working or not");
-    let items = chrome.storage.local.get(["payload"]);
-    console.log("data contains in items", items);
-    console.log("value of payload", payload);
-    // console.log(keyArr);
-    keyIdRemove("name", payload);
-    if (payload.name == undefined)
-      document.getElementById("nameData").style.display = "none";
-  });
+  // document.getElementById("userName")
+  // .addEventListener("click", function () {
+  //   console.log("working or not");
+  //   let items = chrome.storage.local.get(["payload"]);
+  //   console.log("data contains in items", items);
+  //   console.log("value of payload", payload);
+  //   // console.log(keyArr);
+  //   keyIdRemove("name", payload);
+  //   if (payload.name == undefined)
+  //     document.getElementById("nameData").style.display = "none";
+  // });
 
   //Removing the user url
-  document.getElementById("userUrl").addEventListener("click", async function () {
-    console.log("working or not");
-    let items = await chrome.storage.local.get(["payload"]);
-    console.log("data contains in items", items);
-    console.log("value of payload", payload);
-    // console.log(keyArr);
-    keyIdRemove("url", payload);
-    if (payload.url == undefined)
-      document.getElementById("urlData").style.display = "none";
-  });
+  // document.querySelector(".deleteBtn").addEventListener("click", function(e) {
+  //   e.preventDefault();
+  //   console.log("latest value of ee", e);
+  // })
 
   //remove function
-  function keyIdRemove(param, payload) {
-    let keyArr = Object.keys(payload);
-    let obj = {};
-    keyArr.forEach((key) => {
-      if (key != param) {
-        obj[key] = payload[key];
-      }
-    });
-    console.log("object value:::", obj);
-    chrome.storage.local.set({ payload: obj }, function () {
-      console.log("Value is set to " + obj);
-    });
-  }
+  // function keyIdRemove(param, payload) {
+  //   let keyArr = Object.keys(payload);
+  //   let obj = {};
+  //   keyArr.forEach((key) => {
+  //     if (key != param) {
+  //       obj[key] = payload[key];
+  //     }
+  //   });
+  //   console.log("object value:::", obj);
+  //   chrome.storage.local.set({ payload: obj }, function () {
+  //     console.log("Value is set to " + obj);
+  //   });
+  // }
 });
 // }
 
-
-function appendTableHead(){
-  const arr = ["ID", "NAME", "URL"]
+function appendTableBody(payload) {
+  let tbody = "";
   let trHtml = "";
-  arr.forEach((key) => {
-    console.log(key);
-    trHtml +=
-    // `<th>` + key + `</th>`;
-    `<th>${key}</th>`
-    tblHead.innerHTML = trHtml;
+  const keys = Object.keys(payload);
+  console.log("keys value:::", keys);
+  keys.forEach((key) => {
+    trHtml += `<th>${key}</th>`;
+
+    tbody += `<td id="idData">${payload[key]}<button class="deleteBtn" id="${payload[key]}">Delete</button></td>`;
+  });
+  tblHead.innerHTML = trHtml;
+  tblBody.innerHTML = tbody;
+
+  document.querySelectorAll(".deleteBtn").forEach((btns) => {
+    btns.addEventListener("click", function (e) {
+      console.log("getting click or not");
+      const obj = {};
+      keys.forEach((key) => {
+        if (payload[key] != e.srcElement.id) {
+          obj[key] = payload[key];
+        }
+        console.log("value of obj 12122:::", obj);
+        chrome.storage.local.set({ payload: obj }, function () {
+          console.log("Value is set to " + obj);
+          appendTableBody(payload);
+        });
+      });
+    });
   });
 }
-
-function appendTableBody(payload){
- let tbody = '';
- const keys = Object.keys(payload);
- console.log("keys value:::", keys);
- keys.forEach(key => {
-   tbody += `<td id="idData">${key}<button id="userId">Delete</button></td>`
-   tblBody.innerHTML = tbody;
- })
-}
-
-
-
-
 
 /*
 
