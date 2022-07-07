35a88688ef7c57b4eaedc0d45d2407e12ecd2766
Bishal-Tier5 <bishal.shaw@tier5.in>
2022-07-07 20:19:44 +0530

    Work done by bishal

--- dist/content.js
+++ dist/content.js
@@ -1,26 +1,76 @@
-let groupIdContainerString = document.querySelector('a[href*="/photo.php?"]').href;
-groupIdContainerString = groupIdContainerString.split("&id=")[1].split("&")[0];
 
-console.log("groupIdContainerString -> ", groupIdContainerString);
-
-let name  = document.querySelector('div[data-sigil="timeline-cover"]').querySelector('h3').innerHTML;
-let url = window.location.href;
-let userName = url.split("?")[0].split("/")[3];
-
-chrome.storage.local.get(['value'], function(result) {
-    const value = result.value;
-    if(groupIdContainerString === value || name === value || userName === value ){
-        const dataObj = {
-            "Name":name,
-            "URL": userName,
-            "Id": groupIdContainerString
-        }
-        chrome.storage.local.set({"payload": dataObj}, function(res) {
-    
-            console.log('Value is set to ' + res);
+function setParameter(params) {
+    return new Promise((resolve) => {
+        chrome.storage.local.set(params, function (err, data) {
+            if (err) {
+                resolve({
+                    'isError': true
+                })
+            } else {
+                resolve({
+                    'isError': false
+                })
+            }
         });
-    }
-    else{
-        window.alert("The name doesn't match, please provide the correct facebook name");
-    }
+    });
+}
+
+$(document).ready(function(){
+    let userDetails = {};
+
+    chrome.storage.local.get(['value'], function(result) {
+        let profileName = $("#cover-name-root").text();
+        let isNickName = /\([\d]+\)/.test(profileName);
+        let nickName = "";
+
+        if (isNickName) {
+            let getNickName = /\(([^)]+)\)/;
+            let matches = getNickName.exec("Vishal Shaw (Example)");
+            nickName = matches[1];
+            
+            let newProfileName = profileName.split(matches[0]);
+
+            profileName = newProfileName.trim();
+        } else {
+            nickName = 'N/A'
+        }
+
+        let current_url_1 = window.location.href.split('facebook.com/');
+        let current_url_2 = current_url_1[1].split('?');
+
+        let username = "";
+
+        if (current_url_2[0] == "profile.php") {
+            username = "N/A"
+        } else {
+            username = current_url_2[0]
+        }
+
+        let profile_url = $("div#m-timeline-cover-section a:eq(1)").attr("href").split("&");
+        let profile_id = profile_url[1].split("=")
+        let fbId = profile_id[1]
+
+        if (result.value === profileName || 
+            (nickName === 'N/A' && result.value === nickName) ||
+            (username === 'N/A' && result.value === username) ||
+            result.value === fbId) {
+        } else {
+            window.alert("Facebook name has not matched please provide your facebook name")
+            window.close();
+        }
+
+        // for id take id query string from profile pic
+        // Get the value from url after 'facebook.com/' till before '?'. If it is coming as 'profile.php' then username will 'N/A' else it will display username.
+        // If nickname is set then it will show with Profile name in bracket. If their will be no nick name then only profile name will show.
+
+        userDetails = {
+            "profileName": profileName,
+            "fbId": fbId,
+            "userName": username,
+            "nickName": nickName
+        };
+        
+        let params = { 'userDetails': userDetails };
+        setParameter(params);
+    });
 });
--- dist/popup.js
+++ dist/popup.js
@@ -1,15 +1,184 @@
-let submit = document.getElementById("submit");
 
-submit.addEventListener("click", function() {
-    const fb_id = document.getElementById("fb_id").value;
-    if(fb_id == ""){
-        window.alert("Please enter facebook name")
-    }
-    else{
-        window.open('https://facebook.com/me/')
-        chrome.storage.local.set({"value": fb_id}, function(res) {
+function setValues(params) {
+    return new Promise((resolve) => {
+      chrome.storage.local.set(params, function (err, data) {
+          if (err) {
+            resolve({
+                'isError': true
+            })
+          } else {
+            resolve({
+                'isError': false
+            })
+          }    
+      });
+    });
+}
 
-            console.log('Value is set to ' + res);
-        });
+function getValues(params) {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(params, function (data) {
+            resolve({
+                'data': data
+            })  
+      });
+    });
+}
+
+async function showTable () {
+    let details = await getValues(['userDetails']);
+    let userDetails = details.data.userDetails;
+
+    if (userDetails != '' && Object.keys(details.data.userDetails).length != 0) {
+        let table = '<table border="1"><thead><tr><th>Name</th><th>Value</th><th>Action</th></tr></thead>';  
+        if (userDetails.profileName) {
+          table += '<tr><td>Profile Name: </td><td>'+userDetails.profileName+'</td><td><button id="profileName">Delete</button></td></tr>'; 
+        }
+        
+        if (userDetails.fbId) {
+          table += '<tr><td>FB Id: </td><td>'+userDetails.fbId+'</td><td><button id="fbId">Delete</button></td></tr>'; 
+        }
+  
+        if (userDetails.nickName) {
+          table += '<tr><td>Nick Name: </td><td>'+userDetails.nickName+'</td><td><button id="nickName">Delete</button></td></tr>'; 
+        }
+  
+        if (userDetails.userName) {
+          table += '<tr><td>User Name: </td><td>'+userDetails.userName+'</td><td><button id="userName">Delete</button></td></tr>'; 
+        }
+  
+        table += '</table>';
+        $("#changeContent").html(table); 
+    } else {
+        let input = '<input type="text" id="fb_id" name="fb_id" placeholder="Please provide input"><br><br><input type="submit" id="submit" name="submit"></input>';
+        $("#changeContent").html(input);
     }
+}
+
+
+$(function () {
+    showTable ();
+
+    $(document).on("click", "#profileName", function() {
+        let text = "Do you want to delete profile name?";
+        if (!confirm(text)) {
+            return;
+        }
+        let details = await getValues(['userDetails']);
+        console.log("details",details);
+        console.log("details.data.userDetails",details.data.userDetails);
+        let userDetails = {};
+
+        if (details.data.userDetails.fbId) {
+            userDetails.fbId = details.data.userDetails.fbId;
+        }
+    
+        if (details.data.userDetails.nickName) {
+            userDetails.nickName = details.data.userDetails.nickName;
+        }
+    
+        if (details.data.userDetails.userName) {
+            userDetails.userName = details.data.userDetails.userName;
+        }
+
+        var params = { 'userDetails': userDetails };
+        setParameter(params);
+
+        showTable();
+    })
+
+    $(document).on("click", "#fbId", function() {
+        let text = "Do you want to delete facebook Id?";
+        if (!confirm(text)) {
+            return;
+        }
+        
+        let details = getValues(['userDetails']);
+        let userDetails = {};
+
+        if (details.data.userDetails.profileName) {
+            userDetails.profileName = details.data.userDetails.profileName;
+        }
+    
+        if (details.data.userDetails.nickName) {
+            userDetails.nickName = details.data.userDetails.nickName;
+        }
+    
+        if (details.data.userDetails.userName) {
+            userDetails.userName = details.data.userDetails.userName;
+        }
+
+        var params = { 'userDetails': userDetails };
+        setParameter(params);
+
+        showTable();
+    })
+
+    $(document).on("click", "#nickName", function() {
+        let text = "Do you want to delete nick name?";
+        if (!confirm(text)) {
+            return;
+        }
+        
+        let details = getValues(['userDetails']);
+        let userDetails = {};
+
+        if (details.data.userDetails.profileName) {
+            userDetails.profileName = details.data.userDetails.profileName;
+        }
+    
+        if (details.data.userDetails.fbId) {
+            userDetails.fbId = details.data.userDetails.fbId;
+        }
+    
+        if (details.data.userDetails.userName) {
+            userDetails.userName = details.data.userDetails.userName;
+        }
+
+        var params = { 'userDetails': userDetails };
+        setParameter(params);
+
+        showTable();
+    })
+
+    $(document).on("click", "#userName", function() {
+        let text = "Do you want to delete user name?";
+        if (!confirm(text)) {
+            return;
+        }
+        
+        let details = getValues(['userDetails']);
+        let userDetails = {};
+
+        if (details.data.userDetails.profileName) {
+            userDetails.profileName = details.data.userDetails.profileName;
+        }
+    
+        if (details.data.userDetails.fbId) {
+            userDetails.fbId = details.data.userDetails.fbId;
+        }
+    
+        if (details.data.userDetails.nickName) {
+            userDetails.nickName = details.data.userDetails.nickName;
+        }
+
+        var params = { 'userDetails': userDetails };
+        setParameter(params);
+
+        showTable();
+    })
+
+    $("#submit").click(function() {
+        const fb_id = $("#fb_id").val().trim();
+        if(fb_id == ""){
+            window.alert("Please enter facebook name")
+        }
+        else{
+            window.open('https://m.facebook.com/me/')
+            chrome.storage.local.set({"value": fb_id}, function(res) {
+    
+                console.log('Value is set to ' + res);
+            });
+        }
+    })  
 })
